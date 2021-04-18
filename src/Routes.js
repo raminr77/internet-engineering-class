4c0817dba08a0d43e5ad80fbd46b936c4d639414
Ramin <Ramin.rezaei77@gmail.com>
2021-04-18 10:20:53 +0430

    add route

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router , Switch , Route } from 'react-router-dom'
 
+import RaminRezaei from './views/39601620/index'
+
+
 export default function Routes() {
     return (
         <Router>
             <Switch>
 
-                
+                <Route path="/39601620/" component={RaminRezaei} />
 
             </Switch>
         </Router>
