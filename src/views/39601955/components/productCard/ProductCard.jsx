00a307b3bc373f464522affa7219eea001b3d19b
MahdiGhorbani <67688553+oliver19999@users.noreply.github.com>
2021-05-06 03:10:49 +0430

    feat:CartCounter

--- src/views/39601955/components/productCard/ProductCard.jsx
+++ src/views/39601955/components/productCard/ProductCard.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {intCommas, convertToFaDigit} from './../../functions/functions'
+import Header from './../../layouts/Header'
+import  ContextCart from './../../Context/ContextCart'
 
-export default function productCard(props) {
+export default function ProductCard(props) {
+
+    const cart_Contrxt = useContext(ContextCart); 
+
+
+    function AddToCart(id){
+        // cart_Contrxt.Test(t);
+        cart_Contrxt.AddProductToCart(id) 
+        // console.log(cart_Contrxt.listProducts);
+    }
+
     return (
         <div className = "product-card">
             <img src={props.data.image} alt={props.data.title}/>
@@ -10,19 +23,19 @@ export default function productCard(props) {
                     props.data.discount? 
                     <div>
                         <div className="price-box">
-                            <del>{props.data.price}</del>
-                            <span className="discount">{Math.round((props.data.discount * 100) /props.data.price)}%</span>
+                            <del>{intCommas(props.data.price)}</del>
+                            <span className="discount">{ convertToFaDigit(Math.ceil((props.data.discount * 100) /props.data.price))  }%</span>
                         </div>
-                            <p className="price">{props.data.price - props.data.discount}<span style={{fontWeight:"normal",fontSize:"14px"}}> ریال</span></p>
+                            <p className="price">{intCommas(props.data.price - props.data.discount)}<span style={{fontWeight:"normal",fontSize:"14px"}}> ریال</span></p>
                     </div>
 
                     : 
-                    <p className="price">{props.data.price}<span style={{fontWeight:"normal",fontSize:"14px"}}> ریال</span></p>
+                    <p className="price">{intCommas(props.data.price)}<span style={{fontWeight:"normal",fontSize:"14px"}}> ریال</span></p>
 
                 }
             </div>
             <div className="add-to-cart">
-                <button className="btn-product">اضافه به سبد خرید</button>
+                <button onClick={()=> AddToCart(props.data.id)} className="btn-product">اضافه به سبد خرید</button>
             </div>
         </div>
     )
--- src/views/39601955/index.jsx
+++ src/views/39601955/index.jsx
@@ -1,13 +1,21 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useContext} from 'react'
 import Header from './layouts/Header'
 import ProductCard from './components/productCard/ProductCard.jsx'
 import axios from 'axios'
+import  ContextCart from './Context/ContextCart'
 
 import './css/main.css'
 <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v28.0.0/dist/font-face.css" rel="stylesheet" type="text/css" />
 
 export default function Home() {
+
     const [products , setProduct] = useState([])
+    const [listProducts , setListProducts] = useState([])
+
+    function AddProductToCart(item){
+        setListProducts(listProducts.concat(item))
+    }
+
     useEffect(()=>{
         axios.get(`http://raminr77.pythonanywhere.com/api/v1/products/`)
             .then(response => {
@@ -16,22 +24,27 @@ export default function Home() {
             .then(response =>{
               if(!response.data.success) return
               setProduct(response.data.data)
-
             })
             .catch(err=> console.log(err))
     },[])
 
     return (
-        <div>
-            <Header count={3}/>
+        <ContextCart.Provider value={{
+            AddProductToCart : AddProductToCart,
+            listProducts : listProducts,
+            products :products
+        }}>
+                {/* {console.log(listProducts.length)} */}
+                <Header count={listProducts.length}/>
 
-            <div className="products">
-                {
-                products.map((items,index)=>(
-                    <ProductCard key={index} data={items} />
-                ))
-                }
-            </div>
-        </div>
+                <div className="products">
+                    {
+                    products.map((items,index)=>(
+                        <ProductCard key={index} data={items} />
+                    ))
+                    }
+                </div>
+            
+        </ContextCart.Provider>
     )
 }
