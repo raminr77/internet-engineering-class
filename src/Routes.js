4277774dd11e335b3a669efc89c4fb023199303a
Rman <armanazarnik@gmail.com>
2021-07-15 12:40:02 +0430

    add route

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router , Switch , Route } from 'react-router-dom'
+import ArmanAzarnik from './views/39400988/index'
 
 export default function Routes() {
     return (
         <Router>
             <Switch>
 
-                
+                <Route path="/39400988/" component={ArmanAzarnik}/>
 
             </Switch>
         </Router>
